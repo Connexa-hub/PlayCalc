612c0a5490e1468c8f16ca0f8dcde1b762d10347
Connexa-hub <akinsofth@gmail.com>
2025-09-12 15:15:31 +0100

    modified:   app.config.js 	modified:   eas.json 	modified:   yarn.lock

--- app.config.js
+++ app.config.js
@@ -1,40 +1,43 @@
 export default ({ config }) => ({
   ...config,
-  name: "PlayCalc",
-  slug: "PlayCalc",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  userInterfaceStyle: "light",
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
-  ios: {
-    supportsTablet: true,
+  expo: {
+    name: "PlayCalc",
+    slug: "PlayCalc",
+    version: "1.0.0",
+    orientation: "portrait",
     icon: "./assets/icon.png",
-  },
-  android: {
-    package: "com.connexa.playcalc",
-    icon: "./assets/icon.png",
-    adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon-foreground.png",
-      backgroundColor: "#000000",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    edgeToEdgeEnabled: true,
-  },
-  web: {
-    favicon: "./assets/favicon.png",
-  },
-  plugins: [
-    "react-native-google-mobile-ads" // <-- Add the Google Mobile Ads plugin here
-  ],
-  extra: {
-    API_KEY: process.env.API_KEY,
-    eas: {
-      projectId: "f8436d95-c601-4813-b055-90f81ea85d6c",
+    ios: {
+      supportsTablet: true,
+      icon: "./assets/icon.png",
     },
+    android: {
+      package: "com.connexa.playcalc",
+      icon: "./assets/icon.png",
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon-foreground.png",
+        backgroundColor: "#000000",
+      },
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [
+      "react-native-google-mobile-ads" // ✅ Added here
+    ],
+    extra: {
+      API_KEY: process.env.API_KEY,
+      eas: {
+        projectId: "f8436d95-c601-4813-b055-90f81ea85d6c",
+      },
+    },
+    owner: "connexa",
   },
-  owner: "connexa",
 });
