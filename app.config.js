d8f711c6e7cb80b75b9280804a2fd9fa0c585c6a
Connexa-hub <akinsofth@gmail.com>
2025-09-15 16:47:36 +0100

    modified:   app.config.js 	new file:   assets/connexa.png

--- app.config.js
+++ app.config.js
@@ -4,21 +4,21 @@ export default ({ config }) => ({
   slug: "PlayCalc",
   version: "1.0.0",
   orientation: "portrait",
-  icon: "./assets/icon.png",
+  icon: "./assets/icon.png", // Homescreen app icon
   userInterfaceStyle: "light",
   newArchEnabled: true,
   splash: {
-    image: "./assets/splash.png",
+    image: "./assets/connexa.png", // Branding logo before splash screen
     resizeMode: "contain",
     backgroundColor: "#ffffff",
   },
   ios: {
     supportsTablet: true,
-    icon: "./assets/icon.png",
+    icon: "./assets/icon.png", // Keep original icon for iOS homescreen
   },
   android: {
     package: "com.connexa.playcalc",
-    icon: "./assets/icon.png",
+    icon: "./assets/icon.png", // Original homescreen icon
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon-foreground.png",
       backgroundColor: "#000000",
@@ -29,7 +29,6 @@ export default ({ config }) => ({
     favicon: "./assets/favicon.png",
   },
   plugins: [
-    // <-- add this plugin
     "expo-build-properties",
   ],
   extra: {
